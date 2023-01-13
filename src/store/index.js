26855922f93ac1d4a8f939b115446576b91bd2ba
Abhishek <55457501+Abhishek-Dobliyal@users.noreply.github.com>
2023-01-13 21:27:14 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -79,7 +79,7 @@ export default createStore({
       },
     },
     linkToResume:
-      "https://drive.google.com/file/d/1P5x-mB-TRjWtj_gUwYWDgcN1eOwXe2bK/view?usp=share_link",
+      "https://drive.google.com/file/d/1bs8fuvi1jBWzJGZlbdHA62YJRyBH2wno/view?usp=share_link",
   },
   getters: {
     getAboutMeIntroLine(state) {

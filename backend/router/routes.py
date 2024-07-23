d792ad7ef51f18f190a0b8aa89f2ed2b5af514c8
Abhishek <55457501+Abhishek-Dobliyal@users.noreply.github.com>
2024-07-23 21:36:00 +0530

    update route logic

--- backend/router/routes.py
+++ backend/router/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from bson import ObjectId
 
 from database.database import Database
@@ -12,14 +13,21 @@ custom_logger = Logger(__name__).get_logger()
 
 @router.get("/get-stats", response_model=GetResponseModel)
 async def get_stats():
-    document = db.find_one() # Fetch the document from collection
-    if not document:
+    stats = db.find_one() # Fetch the document from collection
+
+    # Set current day visit count to 0 every new day
+    curr_day_cnt = stats["current_day_cnt"]
+    curr_utc_date = datetime.utcnow().strftime("%Y-%m-%d")
+    if curr_utc_date not in curr_day_cnt:
+        stats["current_day_cnt"] = {curr_utc_date: 0}
+    
+    if not stats:
         return GetResponseModel(status_code=status.HTTP_200_OK, 
                                 message="successfully fetched the documents")
 
     return GetResponseModel(status_code=status.HTTP_200_OK, 
                             message="successfully fetched the documents",
-                            data=document)
+                            data=stats)
 
 
 @router.post("/update-stats", response_model=UpdateResponseModel)

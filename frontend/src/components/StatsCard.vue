2a4eff8f3d77cbd084ba5e2cdf418b0cb356126c
Abhishek <55457501+Abhishek-Dobliyal@users.noreply.github.com>
2024-07-26 10:48:58 +0530

    add counter animation

--- frontend/src/components/StatsCard.vue
+++ frontend/src/components/StatsCard.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <div class="row text-center justify-content-center">
       <div class="bg-dark rounded-2 p-2 text-center mx-auto m-3 card">
-        <span class="display-3" :style="`color: ${textColor}`">{{ data }}</span>
+        <span class="display-3" :style="`color: ${textColor}`">{{
+          displayDataComputed
+        }}</span>
         <span class="text-white my-2">{{ title }}</span>
         <span
           class="lead text-white my-1"
@@ -24,6 +26,37 @@ export default {
     data: Number || String,
     subTitle: String,
     subTitleColor: String,
+    animationSpeed: Number,
+  },
+  data() {
+    return {
+      displayData: 0,
+      interval: null,
+      speed: this.animationSpeed ?? 100,
+    };
+  },
+  computed: {
+    displayDataComputed() {
+      if (this.displayData >= this.data) {
+        clearInterval(this.interval);
+      }
+
+      if (Number.isInteger(this.displayData)) {
+        return this.displayData;
+      }
+      return this.displayData.toFixed(2);
+    },
+  },
+  mounted() {
+    let steps = this.data / 10;
+    steps = steps == 0 ? 1 : steps;
+    steps = Number.isInteger(this.data) ? Math.floor(steps) : steps;
+    this.interval = setInterval(() => {
+      console.log(this.displayData);
+      if (this.displayData < this.data) {
+        this.displayData += steps;
+      }
+    }, this.speed);
   },
 };
 </script>

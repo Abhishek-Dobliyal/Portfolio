6a5c2dec546700cc7d17933093d8e896535a4f50
Abhishek <55457501+Abhishek-Dobliyal@users.noreply.github.com>
2024-07-26 21:07:16 +0530

    add session stat

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,66 @@
 <template>
-  <transition
-  enter-active-class="animate__animated animate__fadeInLeft"
+  <div
+    class="container d-flex align-items-center justify-content-center"
+    v-if="isLoading"
   >
+    <PageSpinner title="Loading My Journey"></PageSpinner>
+  </div>
+  <transition enter-active-class="animate__animated animate__fadeInLeft" v-else>
     <router-view />
   </transition>
 </template>
 
 <script>
+import moment from "moment";
+
+import PageSpinner from "@/components/PageSpinner.vue";
+
 export default {
   name: "App",
+  components: {
+    PageSpinner,
+  },
+  data() {
+    return {
+      sessionStartDate: undefined,
+    };
+  },
+  computed: {
+    isLoading() {
+      return this.$store.getters.getLoadingFlag;
+    },
+  },
+  async mounted() {
+    await this.$store.dispatch("fetchStatistics");
+    this.sessionStartDate = moment.utc();
+
+    window.addEventListener("beforeunload", () => {
+      let currStats = structuredClone(this.$store.getters.getStatistics);
+      let timeSpent = moment.utc().diff(this.sessionStartDate, "seconds");
+      let currUTCDate = moment().utc().format("YYYY-MM-DD");
+
+      // Update total visitors count
+      currStats.visitors_cnt++;
+      // Update current day visitor count
+      currStats.current_day_cnt[currUTCDate]++;
+      // Update Avg session time of user
+      currStats.avg_session_seconds = parseFloat(
+        (
+          (currStats.avg_session_seconds + timeSpent) /
+          currStats.visitors_cnt
+        ).toFixed(2)
+      );
+
+      this.$store.commit("setStatistics", currStats);
+      this.$store.dispatch("updateStatistics");
+    });
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("beforeunload", () => {
+      console.log("removed unload event");
+    });
+  },
 };
 </script>
 <style>
